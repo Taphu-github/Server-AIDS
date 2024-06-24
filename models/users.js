d0dc462be3f2f8d6a36b4c1e5e2d5326bb6b4ef4
Tandin Phuntsho <99734455+Taphu-github@users.noreply.github.com>
2024-06-24 14:36:10 +0600

    Update users.js and removed redundant hashing

--- models/users.js
+++ models/users.js
@@ -24,20 +24,6 @@ const userSchema = new mongoose.Schema(
     },
     { timestamps: true }
   );
-
-  // Hash the password before saving it to the database
-userSchema.pre('save', async function (next) {
-    const user = this;
-    if (!user.isModified('password')) return next();
-  
-    try {
-      const salt = await bcrypt.genSalt();
-      user.password = await bcrypt.hash(user.password, salt);
-      next();
-    } catch (error) {
-      return next(error);
-    }
-  });
   
   // Compare the given password with the hashed password in the database
   userSchema.methods.comparePassword = async function (password) {
